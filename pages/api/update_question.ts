f04792584a1c807ba9bfbac97a0c61d8b20f0e4a
Justin Zhang <justinzhang@utexas.edu>
2024-02-23 03:43:17 -0600

    see t

--- pages/api/update_question.ts
+++
@@ -1,9 +0,0 @@
-import prisma from "../../lib/prisma";
-
-export default async function handler(req, res) {
-    if (req.method === "PUT") {
-        res.status(200).json({ "message": "successful PUT!" });
-    } else {
-        res.status(400).json({ "message": "invalid PUT request" });
-    }
-}
