fbeef1d5b8cc0f290e20df7fb44ff69adc2304eb
justzh <142582995+justzh@users.noreply.github.com>
2024-03-26 11:39:19 -0500

    placed list item

--- pages/thebay.tsx
+++ pages/thebay.tsx
@@ -2,6 +2,9 @@ export default function TheBay() {
     return (
         <div>
           <h1>Welcome to The Bay!</h1>
+          <ul>
+            <li></li>
+          </ul>
         </div>
     )
 }
